030a3a2891696011a45ae3fafbfec7b23f2e49c7
DavidHerszberg <herszdavid@gmail.com>
2023-06-27 18:12:26 +0300

    added temperature bias

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import random
 
-def determine_num_of_buyers(num_of_popsicles, num_of_customers, price):
+MIN_PRICE = 1
+MAX_PRICE = 10
+MIN_TEMP = 17
+MAX_TEMP = 42
+CUSTOMERS_PER_DAY = 50
+
+def determine_num_of_buyers(num_of_popsicles, num_of_customers, price, temperature):
     """
     determine the number of buyers, based on pre-existing information
     :param num_of_popsicles:
@@ -8,12 +14,15 @@ def determine_num_of_buyers(num_of_popsicles, num_of_customers, price):
     :param price:
     :return: the number of buyers
     """
+
+    temperature_bias = MIN_PRICE + (temperature - MIN_TEMP) * ((MAX_PRICE-MIN_PRICE) / (MAX_TEMP-MIN_TEMP))
     num_of_buyers = 0
-    for customer in range(num_of_customers):
-        willing_to_pay = random.uniform(1,3)
+    customers = 0
+    while num_of_buyers < num_of_popsicles and customers < CUSTOMERS_PER_DAY:
+        willing_to_pay = random.triangular(MIN_PRICE, MAX_PRICE, temperature_bias)
         if willing_to_pay > price:
-            if num_of_buyers < num_of_popsicles:
-                num_of_buyers += 1
+            num_of_buyers += 1
+        customers += 1
     return num_of_buyers
 
 
@@ -25,7 +34,9 @@ def simulate_day(num_of_popsicles, price, num_of_customers = 100):
     :param num_of_customers:
     :return:
     """
-    num_of_buyers = determine_num_of_buyers(num_of_popsicles, num_of_customers, price)
+    temperature = random.randint(MIN_TEMP, MAX_TEMP)
+    num_of_buyers = determine_num_of_buyers(num_of_popsicles, num_of_customers, price, temperature)
+    print(f"Today's temperature: {temperature}C")
     print(f"Customers today: {num_of_buyers}")
     income = num_of_buyers * price               # Income for the day
     return income
